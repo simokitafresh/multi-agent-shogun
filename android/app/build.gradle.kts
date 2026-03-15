134bdb43b5218241d286ff598828ee637e727b2b
simokitafresh <simokitafresh@gmail.com>
2026-03-16 08:22:05 +0900

    chore(android): bump app versionName to 6.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 12
-        versionName = "5.9"
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
